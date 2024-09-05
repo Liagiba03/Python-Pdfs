b4fdd3691661d0fdff868d52101f43d6859433f8
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2024-09-04 23:37:56 -0600

    Correjido y comentarios

--- Pdf_Python/unir_pdf.py
+++ Pdf_Python/unir_pdf.py
@@ -1,11 +1,15 @@
 import PyPDF2
-
+#Nombre de los archivos
 archivos = ["archivo1.pdf", "archivo2.pdf", "archivo3.pdf"]
-pdf_resultado = "resultado_pdf.pdf"
 
+#Nombre del pdf final
+nom_resultado = "resultado_pdf.pdf"
+
+#Unir pdf
 pdf_final = PyPDF2.PdfFileMerger
 
-for nombre_archivo in archivos:
-    pdf_resultado.append(nombre_archivo)
 
-pdf_resultado.write()
+for nombre_archivo in archivos:
+    pdf_final.append(nombre_archivo)
+
+pdf_final.write(nom_resultado)
